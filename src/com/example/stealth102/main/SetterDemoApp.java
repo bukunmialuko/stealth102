7e75b3db6712edf5072e1e1ab4249b4b4d7d07c1
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-11 00:45:48 +0100

    Printing the properties, set by DI using setter injection

--- src/com/example/stealth102/main/SetterDemoApp.java
+++ src/com/example/stealth102/main/SetterDemoApp.java
@@ -1,5 +1,6 @@
 package com.example.stealth102.main;
 
+import com.example.stealth102.CricketCoach;
 import com.example.stealth102.interfaces.ICoach;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,11 +11,14 @@ public class SetterDemoApp {
                 ClassPathXmlApplicationContext("applicationContext.xml");
 
         // retrieve bean from spring  container
-        ICoach theCoach = context.getBean("myCoach", ICoach.class);
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         // call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
+
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
         // close the context
         context.close();
     }
