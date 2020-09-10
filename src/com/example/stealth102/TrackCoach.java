99ae16d4f98839bcb2822f03166f458f47dbd2c8
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-11 00:18:13 +0100

    Used another bean for thr DI

--- src/com/example/stealth102/TrackCoach.java
+++ src/com/example/stealth102/TrackCoach.java
@@ -1,8 +1,19 @@
 package com.example.stealth102;
 
 import com.example.stealth102.interfaces.ICoach;
+import com.example.stealth102.interfaces.IFortuneService;
 
 public class TrackCoach implements ICoach {
+
+    private IFortuneService fortuneService;
+
+    public TrackCoach() {
+    }
+
+    public TrackCoach(IFortuneService theFortuneService) {
+        this.fortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k";
@@ -10,6 +21,6 @@ public class TrackCoach implements ICoach {
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return  "Just do it ! "+ fortuneService.getFortune();
     }
 }
